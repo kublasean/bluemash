b41fdcbd0133dc26e6d1f66301500b51e2455616
Cloud User <ec2-user@ip-172-31-36-85.us-east-2.compute.internal>
2018-11-26 20:33:29 +0000

    adding scripts

---
+++ data/get_blue.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo starting download.....
+#fastq-dump --fasta SRX2360398
+echo -
+#fastq-dump --fasta SRX2360399
+echo --
+#fastq-dump --fasta SRX2360400
+echo ---
+#fastq-dump --fasta SRX2360401
+echo ----
+#fastq-dump --fasta SRX2360402
+echo -----
+#fastq-dump --fasta SRX2360403
+echo ------
+#fastq-dump --fasta SRX2360404
+echo ------
+#fastq-dump --fasta SRX2360405
+echo -------
+#fastq-dump --fasta SRX2360406
+echo --------
+#fastq-dump --fasta SRX2360407
+echo ---------
+#fastq-dump --fasta SRX2360408
+echo ----------
+#fastq-dump --fasta SRX2360409
+echo -----------
+fastq-dump --fasta SRX2360410
+echo ------------
+fastq-dump --fasta SRX2360411
+echo -------------
+fastq-dump --fasta SRX2360412
+echo --------------
+fastq-dump --fasta SRX2360413
+echo ......done
---
+++ data/get_sketches.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#mash sketch SRX2360398.fasta
+#mash sketch SRX2360399.fasta
+#mash sketch SRX2360400.fasta
+#mash sketch SRX2360401.fasta
+#mash sketch SRX2360402.fasta
+#mash sketch SRX2360403.fasta
+#mash sketch SRX2360404.fasta
+#mash sketch SRX2360405.fasta
+#mash sketch SRX2360406.fasta
+#mash sketch SRX2360407.fasta
+#mash sketch SRX2360408.fasta
+#mash sketch SRX2360409.fasta
+mash sketch SRX2360410.fasta
+mash sketch SRX2360411.fasta
+mash sketch SRX2360412.fasta
+mash sketch SRX2360413.fasta
