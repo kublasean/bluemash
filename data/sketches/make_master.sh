196419cf60b35e813dbc226aa3b7f8ef90800c7c
Cloud User <ec2-user@ip-172-31-36-85.us-east-2.compute.internal>
2018-11-27 01:43:23 +0000

    did rudimentary heatmap, lookin gud

---
+++ data/sketches/make_master.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mash paste master SRX2360398.fasta.msh SRX2360399.fasta.msh SRX2360400.fasta.msh SRX2360401.fasta.msh SRX2360402.fasta.msh SRX2360403.fasta.msh SRX2360404.fasta.msh SRX2360405.fasta.msh SRX2360406.fasta.msh SRX2360407.fasta.msh SRX2360408.fasta.msh SRX2360409.fasta.msh SRX2360410.fasta.msh SRX2360411.fasta.msh SRX2360412.fasta.msh SRX2360413.fasta.msh
